4c6bccd1b0961cd108164b155aa9ba371effb201
t0mmyry4n <74562524+t0mmyry4n@users.noreply.github.com>
2020-12-04 09:40:17 -0800

    stage

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,8 +1,19 @@
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(400, 400);
 }
 
 function draw(){
 	background(220,100,3);
+}
+
+function drawGrid()
+for (var x=-width; x < width; x+=40) {
+		line(x, -height, x, height);
+		text(x, x+1, 12);
+	}
+	for (var y=-height; y < height; y+=40) {
+		line(-width, y, width, y);
+		text(y, 1, y+12);
+	}
 }
