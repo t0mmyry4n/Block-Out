e537e838c4704f53e1fdd1bb1fb43a2c147507c8
t0mmyry4n <74562524+t0mmyry4n@users.noreply.github.com>
2020-12-07 11:19:55 -0800

    Update sketch.js
    movement is not functional yet

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,19 +1,49 @@
+let player
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+  player=new avatar(this.x, this.y, 80)
 }
 
-function draw(){
-  drawGrid()
+function draw() {
+ drawGrid()
+  player.drawAvatar()
+  player.moveAvatar()
 }
 
-function drawGrid()
-for (var x=-width; x < width; x+=40) {
-		line(x, -height, x, height);
-		text(x, x+1, 12);
-	}
-	for (var y=-height; y < height; y+=40) {
-		line(-width, y, width, y);
-		text(y, 1, y+12);
-	}
+function drawGrid() {
+  for (var x = -width; x < width; x += 80) {
+    line(x, -height, x, height);
+    text(x, x + 1, 12);
+  }
+  for (var y = -height; y < height; y += 80) {
+    line(-width, y, width, y);
+    text(y, 1, y + 12);
+  }
+}
+
+class avatar {
+  constructor(x, y, speed) {
+    this.x = 80
+    this.y = 80
+    this.speed = 80
+  }
+  drawAvatar() {
+    fill(0)
+    rect(this.x - 20, this.y - 20, this.x + 20, this.y + 20)
+  }
+  moveAvatar() {
+    if (keyPressed(UP_ARROW)) {
+      this.y -= this.speed
+    }
+    if (keyPressed(DOWN_ARROW)) {
+      this.y += this.speed
+    }
+    if (keyPressed(LEFT_ARROW)) {
+      this.x -= this.speed
+    }
+    if (keyPressed(RIGHT_ARROW)) {
+      this.x += this.speed
+    }
+  }
 }
