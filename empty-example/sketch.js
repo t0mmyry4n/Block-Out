e31ea2605255ce0b275f7e782f229284590ada4a
EmilioPonce23 <emiliocponce@gmail.com>
2020-12-07 19:26:39 -0800

    Update sketch.js

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -4,6 +4,20 @@ function setup() {
 
 function draw() {
   background(220);
-  fill(255,255,255);
-  ellipse(mouseX,mouseY,50,50);
+  drawGrid();
+  fill(200, 50, 40);
+  ellipse(mouseX,mouseY,35,35);
+}
+
+function drawGrid() {
+	stroke(200);
+	fill(120);
+	for (var x=-width; x < width; x+=40) {
+		line(x, -height, x, height);
+		text(x, x+1, 12);
+	}
+	for (var y=-height; y < height; y+=40) {
+		line(-width, y, width, y);
+		text(y, 1, y+12);
+	}
 }
